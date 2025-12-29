8bc4dca5304b19a1dec98feb612e0284fc60c41e
saleem-15 <94238476+saleem-15@users.noreply.github.com>
2025-12-29 12:36:26 +0200

    feat: the app takes full screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:new_nuntium/routes/routes.dart';
@@ -6,6 +7,23 @@ import 'package:new_nuntium/routes/routes.dart';
 import 'core/theme/app_theme.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // ضبط إعدادات النظام لتشمل المساحة العلوية والسفلية
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent, // شفافية الأعلى
+      systemNavigationBarColor: Colors.transparent, // شفافية الأسفل
+      systemNavigationBarIconBrightness:
+          Brightness.dark, // أيقونات الأسفل (سوداء)
+      statusBarIconBrightness:
+          Brightness.dark, // أيقونات الأعلى (سوداء)
+    ),
+  );
+
+  // تفعيل وضع الحافة إلى الحافة (Edge-to-Edge)
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+
   await ScreenUtil.ensureScreenSize();
 
   runApp(const MyApp());
