0f481049cb2907ca8e8e9fe1ed08491157d5baa1
saleem-15 <94238476+saleem-15@users.noreply.github.com>
2025-12-31 14:27:59 +0200

    feat(ui): create reusable AppHeader component for consistent page navigation

---
+++ lib/core/widgets/header.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:new_nuntium/core/extensions/theme_extension.dart';
+
+/// The header of  page in the app
+/// It has a default padding
+/// ```dart
+///padding: EdgeInsets.only(top: 72.h, bottom: 32.h),
+/// ```
+class Header extends StatelessWidget {
+  const Header({
+    super.key,
+    required this.title,
+    required this.subTtitle,
+    this.topPadding,
+    this.bottomPadding,
+    this.horizentalPadding,
+  });
+
+  final String title;
+  final String subTtitle;
+  final double? topPadding;
+  final double? bottomPadding;
+  final double? horizentalPadding;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(
+        top: 72.h,
+        bottom: 32.h,
+        right: horizentalPadding ?? 0,
+        left: horizentalPadding ?? 0,
+      ),
+      child: Column(
+        children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              title,
+              style: context.headline1,
+              textAlign: TextAlign.left,
+            ),
+          ),
+
+          SizedBox(height: 8.h),
+
+          Align(
+            alignment: AlignmentGeometry.topLeft,
+            child: Text(subTtitle, style: context.body1),
+          ),
+        ],
+      ),
+    );
+  }
+}
