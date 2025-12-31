899b87b74c360d6cd5af3d0250c0cd4e932eb9c4
saleem-15 <94238476+saleem-15@users.noreply.github.com>
2025-12-31 17:45:50 +0200

    chore(categories): add mock data to category controller

--- lib/features/categories/controller/categories_controller.dart
+++ lib/features/categories/controller/categories_controller.dart
@@ -1,8 +1,21 @@
 import 'package:get/get.dart';
+import 'package:new_nuntium/core/resources/app_strings.dart';
 
 class CategoriesController extends GetxController {
-  var categories = <String>[].obs;
   var isLoading = false.obs;
+
+  final categories = [
+    AppStrings.sports,
+    AppStrings.politics,
+    AppStrings.life,
+    AppStrings.gaming,
+    AppStrings.animals,
+    AppStrings.nature,
+    AppStrings.food,
+    AppStrings.art,
+    AppStrings.history,
+    AppStrings.fashion,
+  ].obs;
 
   // هذه الدالة التي سنستدعيها من الخارج
   void fetchCategoriesIfNeeded() {
