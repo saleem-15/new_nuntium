6b06987ee356b2b063839b1b0415aaa95fd06a75
saleem-15 <94238476+saleem-15@users.noreply.github.com>
2026-01-16 18:47:41 +0200

    Chore: sync and reorder ar/en translation keys

--- lib/core/resources/app_strings.dart
+++ lib/core/resources/app_strings.dart
@@ -1,117 +1,126 @@
+import 'package:easy_localization/easy_localization.dart';
+
 class AppStrings {
   AppStrings._();
 
   // Onboarding
-  static const String onboardingTitle1 = 'First to know';
-  static const String onboardingSubtitle1 =
-      'All news from all over the world in one place';
+  static String get onboardingTitle1 => tr('firstToKnow');
+  static String get onboardingSubtitle1 => tr('paragraph');
 
-  static const String onboardingTitle2 = 'Real-time Updates';
-  static const String onboardingSubtitle2 =
-      'Stay connected with the latest events as they happen';
+  // ملاحظة: ملف JSON لا يحتوي حالياً على عناوين فرعية لـ Onboarding 2 و 3
+  // سأبقيها كما هي حالياً لتضيفها لاحقاً في ملف JSON
+  static String get onboardingTitle2 => tr('onboarding_title_2');
+  static String get onboardingSubtitle2 => tr('onboarding_subtitle_2');
 
-  static const String onboardingTitle3 = 'Personalized Feed';
-  static const String onboardingSubtitle3 =
-      'Get news tailored to your interests and preferences';
+  static String get onboardingTitle3 => tr('onboarding_title_3');
+  static String get onboardingSubtitle3 => tr('onboarding_subtitle_3');
 
-  static const String next = 'Next';
+  static String get next => tr('next');
 
-  //Welcome
-  static const String nuntium = 'Nuntium';
-  static const String welcomeBody =
-      'All news from all over the world in one place';
-  static const String getStarted = 'Get Started';
+  // Welcome
+  static String get nuntium =>
+      tr('nuntium'); // تأكد من إضافة هذا المفتاح لملف JSON
+  static String get welcomeBody => tr('paragraph');
+  static String get getStarted => tr('getStarted');
 
-  //Login
-  static const String loginTitle = 'Welcome Back 👋';
-  static const String loginSubTitle =
-      'I am happy to see you again. You can continue where you left off by logging in';
-  static const String emailAdress = 'Email Adress';
-  static const String password = 'Password';
-  static const String forgotPassword = 'Forgot Password?';
-  static const String signIn = 'Sign In';
-  static const String or = 'or';
-  static const String signInwithGoogle = 'Sign In with Google';
-  static const String signInwithFacebook = 'Sign In with Facebook';
-  static const String dontHaveAccount = "Don't have an account?";
-  static const String signUp = 'Sign Up';
+  // Login
+  static String get loginTitle => tr('welcomeTitle');
+  static String get loginSubTitle => tr('loginParagraph');
+  static String get emailAdress => tr('emailAddress');
+  static String get password => tr('password');
+  static String get forgotPassword => tr('forgetPassword');
+  static String get signIn => tr('signIn');
+  static String get or => tr('or');
+  static String get signInwithGoogle => tr('google');
+  static String get signInwithFacebook => tr('facebook');
+  static String get dontHaveAccount => tr('dontHaveAnAccount');
+  static String get signUp => tr('signUp');
 
-  //SignUp
-  static const String signUpTitle = 'Welcome to Nuntium 👋';
-  static const String signUpSubTitle =
-      'Hello, I guess you are new around here. You can start using the application after sign up.';
-  static const String userName = 'Username';
-  static const String repeatPassword = 'Repeat Password';
-  static const String haveAnAccount = 'Already have an account?';
+  // SignUp
+  static String get signUpTitle => tr('signUpTitle');
+  static String get signUpSubTitle => tr('signUpParagraph');
+  static String get userName => tr('username');
+  static String get repeatPassword => tr('confirmPasswordHint');
+  static String get haveAnAccount => tr('signUpFooterMessage');
 
-  //Forget Password
-  static const String forgetPasswordTitle = 'Forgot Password 🤔';
-  static const String forgetPasswordSubTitle =
-      'We need your email adress so we can send you the password reset code.';
-  static const String remmeberPassword = 'Remember the password?';
-  static const String tryAgain = 'Try again';
+  // Forget Password
+  static String get forgetPasswordTitle => tr('forget');
+  static String get forgetPasswordSubTitle => tr('forgetParagraph');
+  static String get remmeberPassword =>
+      tr('remember_password'); // غير موجود في JSON
+  static String get tryAgain => tr('try_again'); // غير موجود في JSON
 
-  //Create New Password
-  static const String createNewPasswordTitle =
-      'Create New Password 🔒';
-  static const String createNewPasswordSubTitle =
-      'You can create a new password, please dont forget it too.';
-  static const String newPassword = 'New Password';
-  static const String repeateNewPassword = 'Repeat New Password';
+  // Create New Password
+  static String get createNewPasswordTitle =>
+      tr('createNewPasswordTitle'); // غير موجود في JSON
+  static String get createNewPasswordSubTitle =>
+      tr('createNewPasswordSubTitle'); // غير موجود في JSON
+  static String get newPassword => tr('newPasswordHint');
+  static String get repeateNewPassword => tr('repeatNewPassword');
 
-  //Verification Code
-  static const String verificatonCodeTitle = 'Verification Code ✅';
-  static const String verificatonCodeSubTitle =
-      'You need to enter 4-digit code we send to your email adress.';
-  static const String confirm = 'Confirm';
-  static const String didntReciveEmail = 'Didn’t receive an email?';
-  static const String sendAgain = 'Send again';
+  // Verification Code
+  static String get verificatonCodeTitle => tr('verificationCodeTitle');
+  static String get verificatonCodeSubTitle => tr('verificationCodeParagraph');
+  static String get confirm => tr('confirm');
+  static String get didntReciveEmail => tr('didntRecieveEmail?');
+  static String get sendAgain => tr('sendAgain');
 
-  //Select favorite topics
-  static const String selectFavoriteTopicsTitle =
-      'Select favorite topics';
-  static const String selectFavoriteTopicsSubTitle =
-      'Select some of your favorite topics to let us suggest better news for you.';
-  static const String fashion = '👗   Fashion';
-  static const String art = '🎨   Art';
-  static const String nature = '🌴   Nature';
-  static const String gaming = '🎮   Gaming';
-  static const String politics = '⚖️   Politics';
-  static const String history = '📜   History';
-  static const String food = '🍔   Food';
-  static const String animals = '🐻   Animals';
-  static const String life = '🌞   Life';
-  static const String sports = '🏈   Sports';
+  // Select favorite topics
+  static String get selectFavoriteTopicsTitle =>
+      tr('selectYourFavouriteTopics');
+  static String get selectFavoriteTopicsSubTitle =>
+      tr('selectYourFavouriteTopicsParagraph');
+  // Topics (With Emoji)
+  static String get fashionWithEmoji => tr('fashionWithEmoji');
+  static String get artWithEmoji => tr('artWithEmoji');
+  static String get natureWithEmoji => tr('natureWithEmoji');
+  static String get gamingWithEmoji => tr('gamingWithEmoji');
+  static String get politicsWithEmoji => tr('politicsWithEmoji');
+  static String get historyWithEmoji => tr('historyWithEmoji');
+  static String get foodWithEmoji => tr('foodWithEmoji');
+  static String get animalsWithEmoji => tr('animalsWithEmoji');
+  static String get lifeWithEmoji => tr('lifeWithEmoji');
+  static String get sportsWithEmoji => tr('sportsWithEmoji');
 
-  //Home Page
-  static const String homePageTitle = 'Browse';
-  static const String homePageSubTitle =
-      'Discover things of this world';
-  static const String search = 'Search';
-  static const String recommendedForYou = 'Recommended for you';
-  static const String seeAll = 'See All';
+  // Home Page
+  static String get homePageTitle => tr('browse');
+  static String get homePageSubTitle => tr('homeParagraph');
+  static String get search => tr('search');
+  static String get recommendedForYou =>
+      tr('recommended_for_you'); // غير موجود في JSON
+  static String get seeAll => tr('seeAll'); // غير موجود في JSON
 
-  //Categories Page
-  static const String categoriesPageTitle = 'Categories';
-  static const String categoriesPageSubTitle =
-      'Thousands of articles in each category';
+  static String get fashion => tr('fashion');
+  static String get art => tr('art');
+  static String get nature => tr('nature');
+  static String get gaming => tr('gaming');
+  static String get politics => tr('politics');
+  static String get history => tr('history');
+  static String get food => tr('food');
+  static String get animals => tr('animals');
+  static String get life => tr('life');
+  static String get sports => tr('sports');
+  static String get random => tr('random');
 
-  //Bookmarks Page
-  static const String bookmarksPageTitle = 'Bookmarks';
-  static const String bookmarksPageSubTitle =
-      'Saved articles to the library';
-  static const String noSavedArticles =
-      "You haven't saved any articles yet !";
+  // Categories Page
+  static String get categoriesPageTitle => tr('categories');
+  static String get categoriesPageSubTitle => tr('categoriesParagraph');
 
-  //Profile Page
-  static const String profile = 'Profile';
-  static const String notifications = 'Notifications';
-  static const String language = 'Language';
-  static const String changePassword = 'Change Password';
-  static const String privacy = 'Privacy';
-  static const String termsAndConditions = 'Terms & Conditions';
-  static const String signOut = 'Sign Out';
+  // Bookmarks Page
+  static String get bookmarksPageTitle => tr('bookmarks');
+  static String get bookmarksPageSubTitle => tr('bookmarksParagraph');
+  static String get noSavedArticles => tr('youHaventSavedAnyArticles');
 
-  //Language Page
-  static const String english = 'English';
+  // Profile Page
+  static String get profile => tr('profile');
+  static String get notifications => tr('notifications');
+  static String get language => tr('language');
+  static String get changePassword => tr('changePassword');
+  static String get privacy => tr('privacy');
+  static String get termsAndConditions => tr('termsAndConditions');
+  static String get signOut => tr('signout');
+
+  // Language Page
+  static String get english => tr('english');
+  static String get arabic => tr('arabic');
 }
