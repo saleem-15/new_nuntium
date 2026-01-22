3d249d0c51fa3ed703ffa8d8ee041eb7dcb14fb4
saleem-15 <saleemmahdi10@gmail.com>
2026-01-22 10:37:37 +0200

    feat(home): add Domain and Data layers

---
+++ lib/features/home/domain/use_cases/fetch_news_use_case.dart
@@ -0,0 +1,23 @@
+import 'package:new_nuntium/core/models/article.dart';
+import 'package:new_nuntium/features/home/domain/repository/news_repository.dart';
+
+/// Use Case responsible for fetching news.
+/// It encapsulates the business logic for retrieving articles.
+class FetchNewsUseCase {
+  FetchNewsUseCase(this._newsRepository);
+
+  final NewsRepository _newsRepository;
+
+  /// Callable class method to execute the use case
+  Future<List<Article>> call({
+    required String category,
+    required int page,
+    required int pageSize,
+  }) async {
+    return await _newsRepository.fetchNews(
+      category: category,
+      page: page,
+      pageSize: pageSize,
+    );
+  }
+}
