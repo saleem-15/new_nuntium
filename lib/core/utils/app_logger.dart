1ce84bb26850a6ce195171bd37e3997287cffaf2
saleem-15 <saleemmahdi10@gmail.com>
2026-01-29 15:05:04 +0200

    chore : implement AppLogger, it loggs orgnized and clean logs

---
+++ lib/core/utils/app_logger.dart
@@ -0,0 +1,49 @@
+import 'package:logger/logger.dart';
+
+class AppLogger {
+  static final Logger _logger = Logger(
+    // منع الطباعة في وضع الـ Release للحفاظ على الأداء والأمان
+    filter: ProductionFilter(),
+    printer: PrettyPrinter(
+      methodCount: 2,
+      errorMethodCount: 8,
+      lineLength: 120,
+      colors: true,
+      printEmojis: true,
+
+      // filter out frames that is not my project
+      excludePaths: [
+        'package:flutter',
+        // 'package:get',
+        'package:dart-sdk',
+        'dart:ui',
+        'dart:async',
+        'dart:isolate',
+        // 'dart:',
+        'package:new_nuntium/core/extensions/get_it_extension.dart',
+        'package:new_nuntium/core/utils/app_logger.dart',
+      ],
+    ),
+  );
+
+  static void i(String message) {
+    _logger.i(message);
+  }
+
+  static void d(String message) {
+    _logger.d(message);
+  }
+
+  static void w(String message) {
+    _logger.w(message);
+  }
+
+  static void e(String message, [dynamic error, StackTrace? stackTrace]) {
+    _logger.e(message, error: error, stackTrace: stackTrace);
+
+    // مستقبلاً: يمكن إضافة كود هنا لإرسال الخطأ إلى Firebase Crashlytics
+    // if (kReleaseMode) {
+    //   FirebaseCrashlytics.instance.recordError(error, stackTrace, reason: message);
+    // }
+  }
+}
