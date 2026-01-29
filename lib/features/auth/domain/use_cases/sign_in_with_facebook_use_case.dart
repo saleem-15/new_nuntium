a64195316fbfdcb535a0454e0eab275d0d7ccc2a
saleem-15 <saleemmahdi10@gmail.com>
2026-01-29 10:39:15 +0200

    `Added AppException to unimplemented use cases`

--- lib/features/auth/domain/use_cases/sign_in_with_facebook_use_case.dart
+++ lib/features/auth/domain/use_cases/sign_in_with_facebook_use_case.dart
@@ -1,11 +1,15 @@
+import 'package:new_nuntium/core/errors/app_exception.dart';
+
 import '../repositories/auth_repository.dart';
 
 class SignInWithFacebookUseCase {
+  // ignore: unused_field
   final AuthRepository _authRepository;
 
   SignInWithFacebookUseCase(this._authRepository);
 
   Future<void> call() {
-    return _authRepository.signInWithFacebook();
+    // return _authRepository.signInWithFacebook();
+    throw AppException('Un implemented');
   }
 }
--- lib/features/auth/domain/use_cases/sign_in_with_google_use_case.dart
+++ lib/features/auth/domain/use_cases/sign_in_with_google_use_case.dart
@@ -1,11 +1,15 @@
+import 'package:new_nuntium/core/errors/app_exception.dart';
+
 import '../repositories/auth_repository.dart';
 
 class SignInWithGoogleUseCase {
+  // ignore: unused_field
   final AuthRepository _authRepository;
 
   SignInWithGoogleUseCase(this._authRepository);
 
   Future<void> call() {
-    return _authRepository.signInWithGoogle();
+    // return _authRepository.signInWithGoogle();
+    throw AppException('Un implemented');
   }
 }
