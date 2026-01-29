09730cc7a9f32107b21d8b22426c3e2e236006a2
saleem-15 <saleemmahdi10@gmail.com>
2026-01-29 18:33:05 +0200

    refactor(Global Widgets): :children_crossing: increase the surface area of clickable text in CustomRichText widget

--- lib/core/widgets/custom_rich_text.dart
+++ lib/core/widgets/custom_rich_text.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:new_nuntium/core/extensions/theme_extension.dart';
 import 'package:new_nuntium/core/theme/app_colors.dart';
 import 'package:new_nuntium/core/theme/app_fonts.dart';
@@ -28,13 +28,28 @@ class CustomRichText extends StatelessWidget {
               color: AppColors.blackLighter,
             ),
           ),
-          TextSpan(
-            text: ' $secondText',
-            style: context.body1.copyWith(
-              fontWeight: AppFonts.bold,
-              color: AppColors.blackPrimary,
+
+          WidgetSpan(
+            alignment: PlaceholderAlignment.middle,
+            child: GestureDetector(
+              // Ensures the entire padding is clickable
+              behavior: HitTestBehavior.opaque,
+              onTap: onTap,
+              // Increases surface area
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 6.sp,
+                  vertical: 10.sp,
+                ),
+                child: Text(
+                  ' $secondText',
+                  style: context.body1.copyWith(
+                    fontWeight: AppFonts.bold,
+                    color: AppColors.blackPrimary,
+                  ),
+                ),
+              ),
             ),
-            recognizer: TapGestureRecognizer()..onTap = onTap,
           ),
         ],
       ),
